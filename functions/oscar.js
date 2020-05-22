32c66f6cfff15412db50a958a85f2055b4dfdd30
Joel Cloralt <joel.cloralt@gmail.com>
2020-05-22 14:47:58 -0700

    update the function to receive data and respond

--- functions/oscar.js
+++ functions/oscar.js
@@ -1,9 +1,12 @@
 exports.handler = (event, context, callback) => {
+    console.log("RECEIVING DATA", event.body);
+    let body = JSON.parse(event.body);
+    if (body === null || body === undefined) {
+        //return some default if nothing was passed
+        body = {title: "Assistant to the regional manager", name: "Oscar"}
+    }
     callback(null, {
       statusCode: 200,
-      body:       JSON.stringify({
-          title: "Hello",
-          name: "Oscar" 
-      }),
+      body: JSON.stringify({title: "Assistant to the regional manager", name: body.name}),
     });
   };
