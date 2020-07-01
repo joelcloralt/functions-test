5494d25e955f3bd465c1fa22a7d3e76773ff9c57
Joel Cloralt <joel.cloralt@gmail.com>
2020-06-30 17:53:59 -0700

    fix the empty parameter for this function

--- functions/oscar.js
+++ functions/oscar.js
@@ -1,19 +1,18 @@
 exports.handler = (event, context, callback) => {
     console.log("RECEIVING DATA", event.body);
 
-
-    let body = JSON.parse(event.body);
-
-
+    let body = event.body;
 
     if (body === null || body === undefined) {
         //return some default if nothing was passed
-        body = {title: "Assistant to the regional manager", name: "Oscar"}
+        body = {title: "Assistant to the regional manager", name: "Oscar"}       
+    } else {
+      body = JSON.parse(event.body)
     }
-
+    
 
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify({title: "Assistant to the regional manager", name: body.name || "empty"}),
+      body: JSON.stringify({title: "Assistant to the regional manager", name: body.name}),
     });
   };
