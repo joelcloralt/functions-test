5cea9274fcc6a3e0839f0b0b406012efd5a31ba0
Joel Cloralt <joel.cloralt@gmail.com>
2020-06-30 17:30:19 -0700

    handle empty names

--- functions/oscar.js
+++ functions/oscar.js
@@ -1,12 +1,19 @@
 exports.handler = (event, context, callback) => {
     console.log("RECEIVING DATA", event.body);
+
+
     let body = JSON.parse(event.body);
+
+
+
     if (body === null || body === undefined) {
         //return some default if nothing was passed
         body = {title: "Assistant to the regional manager", name: "Oscar"}
     }
+
+
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify({title: "Assistant to the regional manager", name: body.name}),
+      body: JSON.stringify({title: "Assistant to the regional manager", name: body.name || "empty"}),
     });
   };
